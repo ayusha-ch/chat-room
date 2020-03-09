b5ee8c88c808e075338574f31975f47678cab4cd
beatriz2889 <beatriz.morales@georgebrown.ca>
2020-03-09 19:56:01 -0400

    Room history

--- routes/history.route.js
+++ routes/history.route.js
@@ -5,31 +5,31 @@ const historyRoute = express.Router();
 let History = require("../model/history");
 
 historyRoute.route("/").get((req, res, next) => {
-  History.find((error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.json(data);
-    }
-  });
+    History.find((error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    });
 });
 
 historyRoute.route("/roomhistory").post((req, res, next) => {
-  History.findOne({ chat_room: req.body.roomname }, (err, doc) => {
-    if (err) {
-      return next(err);
-    }
-    res.json(doc);
-  });
+    History.find({ chat_room: req.body.roomname }, (err, doc) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(doc);
+    });
 });
 
 historyRoute.route("/addhistory").post((req, res, next) => {
-  History.create(req.body, (error, historyDoc) => {
-    if (error) {
-      return next(error);
-    }
-    res.json(historyDoc);
-  });
+    History.create(req.body, (error, historyDoc) => {
+        if (error) {
+            return next(error);
+        }
+        res.json(historyDoc);
+    });
 });
 
 module.exports = historyRoute;
